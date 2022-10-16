16ecdfda4c1559d11aabad488ab48302590d8366
Shadman Ali <shadman.ali@live.co.uk>
2022-10-17 00:11:42 +0530

    installs and configures tailwind css

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,7 +1,11 @@
 import styles from './Home.module.css'
 
 const Home = () => {
-  return <h1>Home</h1>
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    </>
+  )
 }
 
 export default Home
