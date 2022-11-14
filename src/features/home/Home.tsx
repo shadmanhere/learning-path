d91a3c55e78be18bd1eabdae4b6d7b658aae3c17
Shadman Ali <shadman.ali@live.co.uk>
2022-11-14 15:08:55 +0530

    adds card for react learning path on home component

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -9,6 +9,23 @@ const Home = () => {
       <section className='overflow-hidden text-gray-700 '>
         <div className='container px-5 py-2 mx-auto lg:pt-12 lg:px-32'>
           <div className='flex flex-wrap -m-1 md:-m-2'>
+            <div className='flex flex-wrap w-1/3'>
+              <div className='w-full p-1 md:p-2'>
+                <a
+                  className=' bg-slate-300 block object-cover object-center w-full h-full rounded-lg'
+                  rel='noreferrer'
+                  href='path/react'
+                >
+                  <h5 className='text-gray-900 text-xl leading-tight font-medium mb-2 align-middle	'>
+                    React Js
+                  </h5>
+                  <p className='text-gray-700 text-base mb-4'>
+                    Some quick example text to build on the card title and make up the bulk of the
+                    card&apos;s content.
+                  </p>
+                </a>
+              </div>
+            </div>
             {tutorials.map((tutorial, i) => {
               return (
                 <div key={i} className='flex flex-wrap w-1/3'>
--- src/features/learningpath/LearningPath.tsx
+++ src/features/learningpath/LearningPath.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import { styled } from '@linaria/react'
+import { reactpath } from '../../data/reactpath'
 
 const LearningPath = () => {
   return (
     <div className='container mx-auto'>
-      <h1>Learning Path</h1>
+      <section className='overflow-hidden text-gray-700 '>
+        <div className='container px-5 py-2 mx-auto lg:pt-12 lg:px-32'>
+          <div className='flex flex-wrap -m-1 md:-m-2'>
+            {reactpath.map((tutorial, i) => {
+              return (
+                <div key={i} className='flex flex-wrap w-1/3'>
+                  <div className='w-full p-1 md:p-2'>
+                    <a target='_blank' rel='noreferrer' href={tutorial.url}>
+                      <img
+                        alt='gallery'
+                        className='block object-cover object-center w-full h-full rounded-lg'
+                        src={tutorial.thumbnail}
+                      />
+                    </a>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
