ba08936e92f32aad29915b873f2d0cb07129de98
Shadman Ali <shadman.ali@live.co.uk>
2022-11-21 16:29:11 +0530

    paths is rendered by map function

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -13,6 +13,7 @@ const Home = () => {
   useEffect(() => {
     dispatch(TutorialsList())
   }, [])
+  const listOfPaths = ['React Js Developer', 'Angular Developer', 'Java Developer']
 
   return (
     <div className='container mx-auto'>
@@ -20,45 +21,21 @@ const Home = () => {
         <div className='container px-5 py-2 mx-auto lg:pt-12 lg:px-32'>
           <h2 className='font-bold text-2xl mb-2'>Paths</h2>
           <div className='flex flex-wrap -m-1 md:-m-2'>
-            <div className='flex flex-wrap w-1/3'>
-              <div className='w-full p-1 md:p-2'>
-                <Link
-                  className='bg-rose-800 block object-cover object-center w-full h-full rounded-lg flex justify-center'
-                  rel='noreferrer'
-                  to='path/react-js-developer'
-                >
-                  <h5 className='text-white text-xl font-medium text-center py-2'>
-                    React Js Developer
-                  </h5>
-                </Link>
-              </div>
-            </div>
-            <div className='flex flex-wrap w-1/3'>
-              <div className='w-full p-1 md:p-2'>
-                <Link
-                  className='bg-rose-800 block object-cover object-center w-full h-full rounded-lg flex justify-center'
-                  rel='noreferrer'
-                  to='path/angular-developer'
-                >
-                  <h5 className='text-white text-xl font-medium text-center py-2'>
-                    Angular Developer
-                  </h5>
-                </Link>
-              </div>
-            </div>
-            <div className='flex flex-wrap w-1/3'>
-              <div className='w-full p-1 md:p-2'>
-                <Link
-                  className='bg-rose-800 block object-cover object-center w-full h-full rounded-lg flex justify-center'
-                  rel='noreferrer'
-                  to='path/java-developer'
-                >
-                  <h5 className='text-white text-xl font-medium text-center py-2'>
-                    Java Developer
-                  </h5>
-                </Link>
-              </div>
-            </div>
+            {listOfPaths.map((path, i) => {
+              return (
+                <div key={i} className='flex flex-wrap w-1/3'>
+                  <div className='w-full p-1 md:p-2'>
+                    <Link
+                      className='bg-rose-800 block object-cover object-center w-full h-full rounded-lg flex justify-center'
+                      rel='noreferrer'
+                      to={'path/' + path.replaceAll(' ', '-').toLowerCase()}
+                    >
+                      <h5 className='text-white text-xl font-medium text-center py-2'>{path}</h5>
+                    </Link>
+                  </div>
+                </div>
+              )
+            })}
           </div>
           <hr className='border-2 my-4 bg-zinc-300 border-zinc-300' />
           <h2 className='font-bold text-2xl mt-4 mb-2'>Tutorials</h2>
