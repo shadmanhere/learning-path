f79d6466395fc327abdc0fb3499516283989c082
Shadman Ali <shadman.ali@live.co.uk>
2022-11-21 18:33:01 +0530

    adds google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
+import ReactGA from 'react-ga'
 import Navbar from './features/navbar/Navbar'
 import Home from './features/home/Home'
 import LearningPath from './features/learningpath/LearningPath'
 import './App.css'
 
 function App() {
+  ReactGA.pageview(window.location.pathname + window.location.search)
+
   return (
     <BrowserRouter>
       <Navbar />
