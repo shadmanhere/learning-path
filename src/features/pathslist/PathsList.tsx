16cb3390595ad67f3d6b4e108686ca261b3e04f9
Shadman Ali <shadman.ali@live.co.uk>
2022-11-22 18:19:01 +0530

    adds loading skeleton to pathslist component

--- src/features/pathslist/PathsList.tsx
+++ src/features/pathslist/PathsList.tsx
@@ -6,6 +6,7 @@ import Paths from './Path'
 
 const PathsList = () => {
   const dispatch = useAppDispatch()
+  const status = useAppSelector(selectStatus)
   useEffect(() => {
     dispatch(GetPathsList())
   }, [])
@@ -13,17 +14,27 @@ const PathsList = () => {
   const listOfPaths: string[] = useAppSelector(selectPathsList)
   return (
     <>
-      {listOfPaths.map((path, i) => {
-        return (
-          <>
-            <Paths
-              key={i}
-              pathName={path}
-              pathUrl={'path/' + path.replaceAll(' ', '-').toLowerCase()}
-            ></Paths>
-          </>
-        )
-      })}
+      {status !== 'idle'
+        ? listOfPaths.map((path, i) => {
+            return (
+              <Paths
+                key={i}
+                pathName={path}
+                pathUrl={'path/' + path.replaceAll(' ', '-').toLowerCase()}
+              ></Paths>
+            )
+          })
+        : [0, 1, 2].map((i) => {
+            return (
+              <div key={i} className='flex flex-wrap w-1/3 animate-pulse'>
+                <div className='w-full p-1 md:p-2'>
+                  <a target='_blank' rel='noreferrer' href='#'>
+                    <div className='block object-cover object-center w-full h-20 md:h-10 rounded-lg bg-slate-300	'></div>
+                  </a>
+                </div>
+              </div>
+            )
+          })}
     </>
   )
 }
