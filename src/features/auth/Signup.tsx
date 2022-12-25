29a9d07c02f6a284599c8575802d0d1dbc49b4bc
Shadman Ali <shadman.ali@live.co.uk>
2022-12-26 04:05:31 +0530

    updates sigup page title

--- src/features/auth/Signup.tsx
+++ src/features/auth/Signup.tsx
@@ -37,7 +37,7 @@ const Signup = () => {
       <div className='container grid h-screen place-items-center mx-auto'>
         <Helmet>
           <meta charSet='utf-8' />
-          <title>Signin | Learning Path</title>
+          <title>Signup | Learning Path</title>
         </Helmet>
         <div className='block p-6 rounded-lg shadow-lg bg-white w-100 max-w-xl'>
           <form>
