d81cc9576bbf08f10ad0303b93a3237cbbcf16a5
Shadman Ali <shadman.ali@live.co.uk>
2022-12-31 18:47:30 +0530

    adds forgot password component

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import './App.css'
 import Signin from './features/auth/Signin'
 import Signup from './features/auth/Signup'
 import NotFound from './features/notfound/NotFound'
+import ForgotPassword from './features/auth/ForgotPassword'
 
 function App() {
   ReactGA.pageview(window.location.pathname + window.location.search)
@@ -20,6 +21,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/signin' element={<Signin />} />
         <Route path='/signup' element={<Signup />} />
+        <Route path='/forgotPassword' element={<ForgotPassword />} />
         <Route path='/path/:path/tutorial/:tutorial' element={<Tutorial />} />
         <Route path='/tutorial/:tutorial' element={<Tutorial />} />
         <Route path='/path/:path' element={<LearningPath />} />
---
+++ src/features/auth/ForgotPassword.tsx
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from 'react'
+
+const ForgotPassword = () => {
+  return <h1>Reset Password</h1>
+}
+
+export default ForgotPassword
