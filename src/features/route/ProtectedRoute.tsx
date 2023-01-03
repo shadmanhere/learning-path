32057ffaf0f9419c29f9987eb1675fad02663fd2
Shadman Ali <shadman.ali@live.co.uk>
2023-01-03 22:49:43 +0530

    removes unused imports and vars from protected route component

--- src/features/route/ProtectedRoute.tsx
+++ src/features/route/ProtectedRoute.tsx
@@ -1,13 +1,11 @@
-import React, { useEffect } from 'react'
-import { Route, Navigate, useLocation } from 'react-router-dom'
-import { selectAuthenticated, selectStatus, selectUser, setFromLocation } from '../auth/authSlice'
+import React from 'react'
+import { Navigate, useLocation } from 'react-router-dom'
+import { selectAuthenticated, setFromLocation } from '../auth/authSlice'
 import { useAppSelector, useAppDispatch } from '../../app/hooks'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
 const ProtectedRoute = ({ isAdmin, children }: any) => {
   const isAuthenticated = useAppSelector(selectAuthenticated)
-  const status = useAppSelector(selectStatus)
-  const user = useAppSelector(selectUser)
   const dispatch = useAppDispatch()
   const location = useLocation()
 
