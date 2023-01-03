4de8d7e00dc74e6334763fc444c6b2935d624755
Shadman Ali <shadman.ali@live.co.uk>
2023-01-04 04:15:31 +0530

    updates not found page meta title

--- src/features/notfound/NotFound.tsx
+++ src/features/notfound/NotFound.tsx
@@ -7,7 +7,7 @@ const NotFound = () => {
       <div className='container grid h-screen place-items-center'>
         <Helmet>
           <meta charSet='utf-8' />
-          <title>Signin | Learning Path</title>
+          <title>404 | Learning Path</title>
         </Helmet>
         <div className='block p-6 rounded-lg shadow-lg bg-white'>
           <h1 className='text-5xl font-extrabold text-rose-700'>404: Page Not Found</h1>
