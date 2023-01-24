2026d5aaca4c5db0cb7a91b9c9fed0d57f581475
Shadman Ali <shadman.ali@live.co.uk>
2023-01-24 18:26:30 +0530

    navbar ui fix for smaller screen

--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   return (
     <nav className='relative w-full flex flex-wrap items-center justify-between py-3 bg-gray-100 text-gray-500 hover:text-gray-700 focus:text-gray-700 shadow-lg'>
       <div className='container-fluid w-full flex flex-wrap items-center justify-between px-6'>
-        <div className='container-fluid flex w-screen'>
+        <div className='container-fluid flex w-screen flex-col sm:flex-row'>
           <Link to='/'>
             <Brand className='text-2xl text-rose-800 font-normal tracking-wide'>
               Learning Path
