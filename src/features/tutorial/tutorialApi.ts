88d2aacee445327d8a263bb50f4a5ade0eaeb555
Shadman Ali <shadman.ali@live.co.uk>
2023-03-20 09:25:34 +0530

    adds get chapter api

--- src/features/tutorial/tutorialApi.ts
+++ src/features/tutorial/tutorialApi.ts
@@ -3,3 +3,8 @@ import { API } from '../../app/api'
 export const getTutorial = async (tutorialId: string) => {
   return await API.get(`tutorial/${tutorialId}`)
 }
+
+export const getChapters = async (tutorialId: string) => {
+  const params = new URLSearchParams([['tutorialId', tutorialId]])
+  return await API.get('chapter', { params })
+}
