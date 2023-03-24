68740ba674bb65b30ff39c6efd52f01c541f7609
Shadman Ali <shadman.ali@live.co.uk>
2023-03-24 20:50:52 +0530

    adds home button to navbar, makes minor changes in navbar ui

--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -17,18 +17,23 @@ const Navbar = () => {
   return (
     <nav className='relative w-full flex flex-wrap items-center justify-between py-3 bg-gray-100 text-gray-500 hover:text-gray-700 focus:text-gray-700 shadow-lg'>
       <div className='container-fluid w-full flex flex-wrap items-center justify-between px-6'>
-        <div className='container-fluid flex w-screen flex-col sm:flex-row'>
+        <div className='container-fluid flex w-screen items-center flex-col sm:flex-row'>
           <Link to='/'>
             <Brand className='text-2xl text-rose-800 font-normal tracking-wide'>
               {process.env.REACT_APP_META_NAME}
             </Brand>
           </Link>
-          <div className='ml-auto space-x-0.5'>
+          <div className='mx-auto sm:mx-1 my-2 sm:ml-auto space-x-0.5'>
+            <Link
+              to='/'
+              type='button'
+              className='px-6 py-2.5 bg-gray-200 text-gray-700 font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-gray-300 hover:shadow-lg focus:bg-gray-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out'
+              onClick={() => logoutHandle()}
+            >
+              Home
+            </Link>
             {user.id ? (
               <>
-                <Link to='/me' className='mx-1 font-serif'>
-                  {user.firstName}
-                </Link>
                 <button
                   type='button'
                   className='px-6 py-2.5 bg-gray-200 text-gray-700 font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-gray-300 hover:shadow-lg focus:bg-gray-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out'
@@ -36,6 +41,12 @@ const Navbar = () => {
                 >
                   Logout
                 </button>
+                <Link
+                  to='/me'
+                  className='px-6 py-2.5 bg-gray-200 text-gray-700 font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-gray-300 hover:shadow-lg focus:bg-gray-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out'
+                >
+                  {user.firstName}
+                </Link>
               </>
             ) : (
               <>
