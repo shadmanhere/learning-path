631be53a174a18a4abdef5ce4fac96c287cfd315
Shadman Ali <shadman.ali@live.co.uk>
2023-03-25 12:11:05 +0530

    bug fix: removes logout from home button on navbar

--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -28,7 +28,6 @@ const Navbar = () => {
               to='/'
               type='button'
               className='px-6 py-2.5 bg-gray-200 text-gray-700 font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-gray-300 hover:shadow-lg focus:bg-gray-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out'
-              onClick={() => logoutHandle()}
             >
               Home
             </Link>
