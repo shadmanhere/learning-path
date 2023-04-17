047d5ebc27ebd26ee0c5bcdda78201958c75c7e6
Shadman Ali <shadman.ali@live.co.uk>
2023-04-17 13:32:59 +0530

    minor fix

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -273,10 +273,10 @@ export const authSlice = createSlice({
       })
       .addCase(LoadUser.fulfilled, (state, action) => {
         state.status = 'idle'
+        state.success = true
         if (action.payload.success) {
           state.user = action.payload.user
           state.isAuthenticated = true
-          state.success = action.payload.success
         } else {
           state.error.message = action.payload.data.message
           state.error.statusCode = action.payload.status
