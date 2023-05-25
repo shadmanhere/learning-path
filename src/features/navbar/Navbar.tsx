5050bf830b084d53ae7aeee106e7fa979f56f37f
Shadman Ali <shadman.ali@live.co.uk>
2023-05-25 18:50:00 +0530

    add new logo

--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -54,6 +54,7 @@ const Navbar = () => {
     <nav className='relative w-full flex flex-wrap items-center justify-between py-3 bg-gray-100 text-gray-500 hover:text-gray-700 focus:text-gray-700 shadow-lg'>
       <div className='container-fluid w-full flex flex-wrap items-center justify-between px-6'>
         <div className='container-fluid flex w-screen items-center flex-col sm:flex-row'>
+          <img src='/logo.png' alt='Your Logo' className='w-20 h-20 px-0 mx-0' />
           <Link
             to='/'
             className={`${styles.brand} text-2xl text-rose-800 font-normal tracking-wide`}
